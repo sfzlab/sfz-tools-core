4df06f8201b976989dc1cb2c792fa26fb05b1d29
Kim T <kimturley@gmail.com>
2023-11-25 21:47:15 -0800

    Windows test fixes

--- tests/analyze.test.ts
+++ tests/analyze.test.ts
@@ -19,42 +19,44 @@ const audioFiles: string[][] = audioPaths.map((audioFile: string) => {
   return [path.normalize(audioFile), audioFile];
 });
 
-test.each(audioFiles)('Analyze Danceability %p', async (audioNorm: string, audioFile: string) => {
+console.log(audioFiles);
+
+test.each(audioFiles)('Analyze Danceability %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeDanceability(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Duration %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Duration %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeDuration(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Energy %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Energy %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeEnergy(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Key %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Key %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeKey(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Loudness %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Loudness %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeLoudness(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Notes %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Notes %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeNotes(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Scale %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Scale %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeScale(file)).toMatchSnapshot();
 });
 
-test.each(audioFiles)('Analyze Speed %p', async (audioNorm: string, audioFile: string) => {
+test.each(audioFiles)('Analyze Speed %s', async (audioNorm: string, audioFile: string) => {
   const file: AnalyzeFile = analyzeLoad(audioFile);
   expect(analyzeSpeed(file)).toMatchSnapshot();
 });
