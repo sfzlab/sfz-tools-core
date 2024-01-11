9d991de2eaf9e300f5ec83d7d4f1475eeafab6e9
Kim T <kimturley@gmail.com>
2024-01-10 22:53:32 -0800

    Add yaml conversion

--- src/convert.ts
+++ src/convert.ts
@@ -1,5 +1,6 @@
 import { js2xml, xml2js } from 'xml-js';
 import { parseSetLoader, parseSfz } from './parse';
+import { load, dump } from 'js-yaml';
 import { ParseDefinition, ParseHeader, ParseOpcode } from './types/parse';
 import { LINE_END, normalizeXml, pathGetDirectory, pathGetExt } from './utils';
 import { ConvertOptions } from './types/convert';
@@ -46,6 +47,10 @@ function convertJsToSfz(jsObj: ParseDefinition) {
   return sfzText;
 }
 
+function convertJsToYaml(jsObj: ParseDefinition) {
+  return dump(jsObj);
+}
+
 function convertJsToXml(jsObj: ParseDefinition) {
   const xml: string = js2xml(jsObj, OPTIONS_XML);
   return normalizeXml(xml);
@@ -60,6 +65,11 @@ async function convertSfzToJs(sfzFile: string, prefix = '', localFileFunc?: any)
   };
 }
 
+async function convertSfzToYaml(sfzFile: string, prefix = '') {
+  const sfzJs: any = await convertSfzToJs(sfzFile, prefix);
+  return convertJsToYaml(sfzJs);
+}
+
 async function convertSfzToXml(sfzFile: string, prefix = '') {
   const elements: any = await parseSfz(sfzFile, prefix);
   const xml: string = js2xml(
@@ -72,6 +82,10 @@ async function convertSfzToXml(sfzFile: string, prefix = '') {
   return normalizeXml(xml);
 }
 
+function convertYamlToJs(yamlFile: string) {
+  return load(yamlFile);
+}
+
 function convertXmlToJs(xmlFile: string) {
   return xml2js(xmlFile, OPTIONS_JSON);
 }
@@ -81,4 +95,9 @@ function convertXmlToSfz(xmlFile: string) {
   return convertJsToSfz(jsObj);
 }
 
-export { convert, convertJsToSfz, convertJsToXml, convertSfzToJs, convertSfzToXml, convertXmlToJs, convertXmlToSfz };
+function convertXmlToYaml(xmlFile: string) {
+  const jsObj: ParseDefinition = xml2js(xmlFile, OPTIONS_JSON) as ParseDefinition;
+  return convertJsToYaml(jsObj);
+}
+
+export { convert, convertJsToSfz, convertJsToYaml, convertJsToXml, convertSfzToJs, convertSfzToYaml, convertSfzToXml, convertYamlToJs, convertXmlToJs, convertXmlToSfz, convertXmlToYaml };
--- tests/convert.test.ts
+++ tests/convert.test.ts
@@ -2,10 +2,14 @@ import path from 'path';
 import {
   convertJsToSfz,
   convertJsToXml,
+  convertJsToYaml,
   convertSfzToJs,
   convertSfzToXml,
+  convertSfzToYaml,
   convertXmlToJs,
   convertXmlToSfz,
+  convertXmlToYaml,
+  convertYamlToJs,
 } from '../src/convert';
 import { fileReadJson, fileReadString } from '../src/file';
 import { ParseDefinition } from '../src/types/parse';
@@ -13,10 +17,15 @@ import { ParseDefinition } from '../src/types/parse';
 const syntaxDir: string = path.join('test', 'syntax');
 const sfzJs: ParseDefinition = fileReadJson(path.join(syntaxDir, 'basic.json'));
 const sfzText: string = fileReadString(path.join(syntaxDir, 'basic.sfz'));
+const sfzYaml: string = fileReadString(path.join(syntaxDir, 'basic.yaml'));
 const sfzXml: string = fileReadString(path.join(syntaxDir, 'basic.xml'));
 
 test('Convert Js to Sfz', async () => {
   expect(await convertJsToSfz(sfzJs)).toEqual(sfzText);
+});
+
+test('Convert Js to Yaml', async () => {
+  expect(await convertJsToYaml(sfzJs) + '\n').toEqual(sfzYaml);
 });
 
 test('Convert Js to Xml', async () => {
@@ -27,12 +36,24 @@ test('Convert Sfz to Js', async () => {
   expect(await convertSfzToJs(sfzText, syntaxDir, fileReadString)).toEqual(sfzJs);
 });
 
+test('Convert Sfz to Yaml', async () => {
+  expect(await convertSfzToYaml(sfzText, syntaxDir) + '\n').toEqual(sfzYaml);
+});
+
 test('Convert Sfz to Xml', async () => {
   expect(await convertSfzToXml(sfzText, syntaxDir)).toEqual(sfzXml);
 });
 
+test('Convert Yaml to Js', async () => {
+  expect(await convertYamlToJs(sfzYaml)).toEqual(sfzJs);
+});
+
 test('Convert Xml to Js', async () => {
   expect(await convertXmlToJs(sfzXml)).toEqual(sfzJs);
+});
+
+test('Convert Xml to Yaml', async () => {
+  expect(await convertXmlToYaml(sfzXml) + '\n').toEqual(sfzYaml);
 });
 
 test('Convert Xml to Sfz', async () => {
