a310db9a49b496420a7c0e265566badbfcc6b08d
Kim T <kimturley@gmail.com>
2024-02-10 23:13:50 -0800

    Add quotes to helper methods

--- src/encode.ts
+++ src/encode.ts
@@ -21,32 +21,32 @@ function encode(filepath: string, options: EncodeOptions) {
 
 function encodeFlacToOgg(input: string, output?: string) {
   if (!output) output = input.replace('.flac', '.ogg');
-  return encodeRun(`-i ${input} ${output}`);
+  return encodeRun(`-i "${input}" "${output}"`);
 }
 
 function encodeFlacToWav(input: string, output?: string) {
   if (!output) output = input.replace('.flac', '.wav');
-  return encodeRun(`-i ${input} ${output}`);
+  return encodeRun(`-i "${input}" "${output}"`);
 }
 
 function encodeOggToFlac(input: string, output?: string) {
   if (!output) output = input.replace('.ogg', '.flac');
-  return encodeRun(`-i ${input} ${output}`);
+  return encodeRun(`-i "${input}" "${output}"`);
 }
 
 function encodeOggToWav(input: string, output?: string) {
   if (!output) output = input.replace('.ogg', '.wav');
-  return encodeRun(`-i ${input} ${output}`);
+  return encodeRun(`-i "${input}" "${output}"`);
 }
 
 function encodeWavToFlac(input: string, output?: string) {
   if (!output) output = input.replace('.wav', '.flac');
-  return encodeRun(`-i ${input} ${output}`);
+  return encodeRun(`-i "${input}" "${output}"`);
 }
 
 function encodeWavToOgg(input: string, output?: string) {
   if (!output) output = input.replace('.wav', '.ogg');
-  return encodeRun(`-i ${input} ${output}`);
+  return encodeRun(`-i "${input}" "${output}"`);
 }
 
 function encodeRun(command: string): string {
